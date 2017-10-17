bfd43d43918772ec0ae38ede1350a8b1b5feee01
Razorflame3912 <melsharawy@stuy.edu>
2017-10-17 12:27:05 -0400

    FINALLY DID INSERT_ORDER

--- LLstruct.c
+++ LLstruct.c
@@ -9,13 +9,15 @@ struct node {
     struct node *next;
 };
 
+struct node *HEAD = NULL;
+
 void print_list(struct node *head){
   struct node *tmp = head;
   while(tmp){
     printf("%s by %s, ",tmp->name, tmp->artist);
     tmp = tmp->next;
   }
-  printf("\n");
+  printf("\n\n");
 }
 
 
@@ -24,6 +26,7 @@ struct node* insert_front(struct node *head, char *newname, char *newart){
   strcpy(new_node->name,newname);
   strcpy(new_node->artist, newart);
   new_node->next = head;
+  HEAD = new_node;
   return new_node;
 }
 
@@ -94,31 +97,91 @@ struct node* random_pick(struct node *head){
 
 }
 
+struct node* insert_order(struct node *head, char *newname, char *newart ){
+  if(!head){
+    return insert_front(head,newname,newart);
+  }
+  struct node *follow = NULL;
+  struct node *new_node = (struct node*)malloc(sizeof(struct node));
+  strcpy(new_node->name,newname);
+  strcpy(new_node->artist, newart);  
+  while(head){
+    if(strcmp(new_node->artist,head->artist) > 0){
+
+    }
+    else if(strcmp(new_node->artist,head->artist) < 0){
+      new_node->next = head;
+      if(follow){
+	follow->next = new_node;
+      }
+      else{
+	HEAD = new_node;
+      }
+      return new_node;
+    }
+    
+    else{
+      if(strcmp(new_node->name,head->name) > 0){
+
+      }
+      
+      else if(strcmp(new_node->name,head->name) <= 0){
+
+	new_node->next = head;
+	if(follow){
+	  follow->next = new_node;
+	}
+	else{
+	  HEAD = new_node;
+	}
+	return new_node;
+      }
+
+    }
+
+    follow = head;
+    head = head->next;
+    //printf("%s\n",follow->artist);
+    if(head){
+      //printf("%s\n---\n",head->artist);
+    }
+      
+
+  }
+  follow->next = new_node;
+  new_node->next = NULL;
+  return new_node;
+
+}
+
 
 int main(){
   srand(time(NULL));
-  struct node *head = NULL;
-  head = insert_front(head, "Demons", "Imagine Dragons");
-  head = insert_front(head, "Hello", "Adele");
-  head = insert_front(head, "Baby", "Justin Bieber");
-  print_list(head);
+  insert_order(HEAD, "Demons", "Imagine Dragons");
+  print_list(HEAD);
+  insert_order(HEAD, "Hello", "Adele");
+  print_list(HEAD);
+  insert_order(HEAD, "Baby", "Justin Bieber");
+  print_list(HEAD);
+  insert_order(HEAD, "Radioactive", "Imagine Dragons");
+  print_list(HEAD);
   printf("Looking for Hello...\n");
-  print_list(find_name(head,"Hello"));
+  print_list(find_name(HEAD,"Hello"));
   printf("Looking for Chandelier...\n");
-  print_list(find_name(head,"Chandelier"));
+  print_list(find_name(HEAD,"Chandelier"));
   printf("Looking for Imagine Dragons...\n");
-  print_list(find_artist(head,"Imagine Dragons"));
+  print_list(find_artist(HEAD,"Imagine Dragons"));
   printf("Looking for Maroon 5...\n");
-  print_list(find_artist(head,"Maroon 5"));
-  printf("Length of list is %d\n", list_len(head));
+  print_list(find_artist(HEAD,"Maroon 5"));
+  printf("Length of list is %d\n", list_len(HEAD));
 
   int n = 0;
   for(n;n<10;n++){
-    printf("%s\n",random_pick(head)->name);
+    printf("%s\n",random_pick(HEAD)->name);
   }
-  head = free_list(head);
+  HEAD = free_list(HEAD);
   //Test call of print_list to show free_list worked.
   printf("Printing freed list...\n");
-  print_list(head);
+  print_list(HEAD);
   return 0;
 }
