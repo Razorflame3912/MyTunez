8101aa4679645634bd9ffc9d768bdb105fd4e210
srafi1 <shakilrafi0@gmail.com>
2017-10-18 12:36:17 -0400

    tunez

---
+++ mytunez.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+#include "LLstruct.h"
+
+int main() {
+    struct node table[26];
+    int i;
+    for (int i = 0; i < 26; i++) {
+        table[i] = NULL;
+    }
+    return 0;
+}
