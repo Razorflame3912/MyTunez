1eed17a65863b92e77aa736148febf58d876df4c
Razorflame3912 <melsharawy@stuy.edu>
2017-10-18 21:22:50 -0400

    started library, seg fault when doing print artist songs, idk why yet

--- LLstruct.c
+++ LLstruct.c
@@ -17,11 +17,12 @@ void to_lowercase(char str[]) {
     }
 }
 
+
 void print_list(struct node *head){
     struct node *tmp = head;
     while(tmp){
-        printf("%s by %s, ",tmp->name, tmp->artist);
-        tmp = tmp->next;
+      printf("%s by %s, ",tmp->name,tmp->artist);
+      tmp = tmp->next;
     }
     printf("\n");
 }
@@ -206,6 +207,46 @@ struct node* remove_node(struct node *head, struct node *to_remove) {
     }
 }
 
+struct node* lib_add(struct node *arr[27],char *newname, char *newart){
+  int i = 0;
+  struct node* *tmp = arr;
+  for(i;i<27;i++){
+    if (newart[0] - (97 + i) == 0){
+      tmp[i] = insert_order(tmp[i], newname, newart);
+      return tmp[i];
+    }
+  }
+  return NULL;
+}
+
+struct node* lib_artist_pick(struct node *arr[27],char *artistname){
+  int i = 0;
+  for(i;i<27;i++){
+    if(artistname[0] - (97+ i) == 0){
+      return find_artist(arr[i],artistname);
+    }
+  }
+}
+
+void print_artist_songs(struct node *arr[27], char *artistname){
+  printf("hi");
+  struct node *start = lib_artist_pick(arr,artistname);
+  while(strcmp(artistname,start->artist) == 0){
+    printf("%s by %s, ",start->name,start->artist);
+    start = start -> next;
+  }
+  printf("\n");
+
+}
+
+void print_lib(struct node *arr[27]){
+  int i = 0;
+  for(i;i<26;i++){
+    printf("%c songs:\n\t",97 + i);
+    print_list(arr[i]);
+  }
+}
+
 int main(){
     srand(time(NULL));
     struct node *head = NULL;
@@ -252,5 +293,35 @@ int main(){
     printf("removing bohemian rhapsody...\n");
     head = remove_node(head, find_name(head, "bohemian rhapsody"));
     print_list(head);
+    
+    struct node * lib[27];
+    lib[26] = 0;
+    int i = 0;
+    for(i;i<26;i++){
+      lib[i] = NULL;
+    }
+
+    lib_add(lib,"demons","imagine dragons");
+    //print_lib(lib);
+    lib_add(lib,"radioactive","imagine dragons");
+    lib_add(lib,"believer","imagine dragons");
+    lib_add(lib,"demons","imagine dragons");
+    lib_add(lib,"hello","adele");
+    lib_add(lib,"best day of my life","american authors");
+    lib_add(lib,"classic","mkto");
+    lib_add(lib,"thank you","mkto");
+    lib_add(lib,"baby","justin bieber");
+    lib_add(lib,"never say never","justin bieber");
+    lib_add(lib,"bailando","enrique iglesias");
+    lib_add(lib,"centuries","fallout boy");
+    lib_add(lib,"wavin' flag","k'naan");
+    lib_add(lib,"a thousand miles","vanessa carlton");
+    lib_add(lib,"thriller","michael jackson");
+    lib_add(lib,"can't help falling in love with you","twenty one pilots");
+    print_lib(lib);
+    print_list(lib_artist_pick(lib,"mkto"));
+    print_artist_songs(lib,"justin bieber");
+    
+    
     return 0;
 }
